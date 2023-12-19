8d41382f3faec49e51393dd60c7a85c83eb2b3a1
TorySnopl <Paramedic064@gmail.com>
2023-12-19 15:06:45 -0800

    small server.js change

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,20 +14,20 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const thisDb = await openDB('jate', 12);
+  const thisDb = await openDB('jate', 1);
   const tx = thisDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: 12, value: content});
+  const request = store.put({id: 1, value: content});
   const result = await request;
   console.log('Your content has been successfully added to the database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const thisDb = await openDB('jate', 12);
+  const thisDb = await openDB('jate', 1);
   const tx = thisDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get(12);
+  const request = store.get(1);
   const result = await request;
   console.log('Poof, all the content from the database', result);
   return result?.value;
