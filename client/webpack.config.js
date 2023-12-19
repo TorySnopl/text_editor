915a5c863fb024e1bc58774b3a6bd2912f1336fc
TorySnopl <Paramedic064@gmail.com>
2023-12-19 12:55:19 -0800

    webpack.configure and scripts in package.json done

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,9 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const wrkBox = require ('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
+const cssPlugin = require ('mini-css-extract-plgin');
 
 module.exports = () => {
   return {
@@ -18,12 +20,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html'
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'A useful offline text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        display: 'standalone',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          }
+        ]
+
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new cssPlugin({
+        filename: './src/css/style.css'
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [cssPlugin.loader, 'css-loader'],
+        },
       ],
     },
   };
